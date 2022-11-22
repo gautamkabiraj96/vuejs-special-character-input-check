86a2e8c2702624873abe24eef074e84b7dd41621
Gautam Kabiraj <51134142+gautamkabiraj96@users.noreply.github.com>
2022-11-23 00:58:50 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# vuejs-special-character-input-check
-Created with CodeSandbox
+# Special characters input check in Vue.js
+
+Learn how to check if input contains special characters in Vue.js | Example | Demo [Learn more](https://www.nightprogrammer.com/vue-js/check-if-input-contains-special-characters-in-vue-js-example/)
---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <input
+      type="text"
+      placeholder="Type something..."
+      v-model="inputValue"
+      @keyup="inputContainsSpecialCharacters"
+    />
+    <p>
+      Contains special character:
+      <span v-if="inputContainsSpecialCharacter">✅ True</span>
+      <span v-else>❌ False</span>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      inputValue: null,
+      inputContainsSpecialCharacter: false,
+    };
+  },
+  methods: {
+    inputContainsSpecialCharacters() {
+      const specialCharacters = /[`!@#$%^&*()_+\-=\\|,.<>?~]/;
+      this.inputContainsSpecialCharacter = specialCharacters.test(
+        this.inputValue
+      );
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #000000;
+  margin-top: 60px;
+}
+input {
+  font-size: 16px;
+  padding: 4px;
+  border: 1px solid rgb(34, 34, 34);
+  outline: none;
+}
+</style>
